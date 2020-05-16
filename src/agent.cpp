5580504cd11c021d5ab1793c1a5fa19ddd23c794
Laurent Morichetti <laurent.morichetti@amd.com>
2020-05-16 01:59:30 -0700

    Ignore events for unsupported queues
    Non-AQL queues may have been seen by a queue_snapshot, and ignored. An
    event delivered to these queues may not have the NEW_QUEUE bit set, so
    until we can support non-AQL queues, relax the assertion that a queue_t
    instance should exits if the NEW_QUEUE bit isn't set.
    
    Change-Id: Ic2cf161893a1e9b484cd19eb1cc66b0d27a48e7c

--- src/agent.cpp
+++ src/agent.cpp
@@ -127,10 +127,6 @@ agent_t::next_kfd_event (amd_dbgapi_queue_id_t *queue_id,
              destroyed it if it isn't a supported queue type.  */
           if (process.find (*queue_id))
             return AMD_DBGAPI_STATUS_SUCCESS;
-
-          dbgapi_log (AMD_DBGAPI_LOG_LEVEL_INFO,
-                      "skipping event for deleted kfd_queue_id %d",
-                      kfd_queue_id);
         }
       else if (queue)
         {
@@ -139,11 +135,19 @@ agent_t::next_kfd_event (amd_dbgapi_queue_id_t *queue_id,
         }
       else
         {
-          /* KFD did not report the new queue?  */
+          /* FIXME: Is it possible to reach here if the queue type isn't AQL.
+             We could have seen the queue before and ignored it.  Re-enable
+             this error when queue_t instances are created for all queue types.
+
           error ("kfd_queue_id %d should have been reported as a NEW_QUEUE "
                  "before",
                  kfd_queue_id);
+          */
         }
+
+      dbgapi_log (AMD_DBGAPI_LOG_LEVEL_INFO,
+                  "skipping event for deleted/unsupported kfd_queue_id %d",
+                  kfd_queue_id);
     }
 }
 
